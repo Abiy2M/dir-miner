dfc05af267f1c6f7ddc229a158d618f3fe7dfca8
Lenovo <it.abiym22@gmail.com>
2023-06-09 17:39:28 +0300

    Fix: fixed the issue with github repos

--- dir-miner.sh
+++ dir-miner.sh
@@ -12,19 +12,31 @@ output_file="$2"
 # Check if the repository link starts with "https://" to determine if it's a GitHub link
 if [[ $repository_link_or_path == https://* ]]; then
     # Clone the repository
-    git clone "$repository_link_or_path" repo
+    if ! git clone "$repository_link_or_path" repo; then
+        echo "Failed to clone the GitHub repository."
+        exit 1
+    fi
+
+    # Navigate to the cloned repository directory
+    cd repo
 else
     # Use the provided path for local repository
     repo="$repository_link_or_path"
-fi
 
-# Navigate to the cloned repository directory
-cd "$repo"
+    # Check if the local repository directory exists
+    if [ ! -d "$repo" ]; then
+        echo "The provided local repository path does not exist."
+        exit 1
+    fi
+
+    # Navigate to the local repository directory
+    cd "$repo"
+fi
 
 # Find all files in the directory and write their relative paths to the output file
 find . -type f | sed 's/^\.\///' > "../$output_file"
 
-# Move back to the original directory if a GitHub repository was cloned (Comment the following block out, if you want to keep the github repo)
+# Move back to the original directory if a GitHub repository was cloned
 cd ..
 if [[ $repository_link_or_path == https://* ]]; then
     rm -rf repo
